b4121c3e3d2006b5ec61d287c0f406a2e81aba1a
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-13 18:45:47 -0500

    file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hola");
+import "./components/export"
+
+class AppContainer extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"})
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+    render() {
+        const something = this.ownerDocument.createElement('div');
+        this.shadowRoot?.appendChild(something);
+    }
+}
+
+customElements.define('app-container', AppContainer)
